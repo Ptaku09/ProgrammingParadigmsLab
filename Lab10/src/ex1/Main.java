7f43430cf09a220c65b7ae617dd3fbf82cc02363
Mateusz Ptak <ptakm02@gmail.com>
2023-01-18 23:48:50 +0100

    lab 10 - exercise 1

---
+++ Lab10/src/ex1/Main.java
@@ -0,0 +1,69 @@
+package ex1;
+
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main {
+    private static final AtomicInteger GLOBAL_VARIABLE = new AtomicInteger(0);
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final Random RAND = new Random();
+
+    public static void main(String[] args) {
+        int numberOfThreads = readNumberOfThreads();
+        int duration = readDuration();
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            new Thread(() -> {
+                while (true) {
+                    int operation = RAND.nextInt(2);
+
+                    synchronized (GLOBAL_VARIABLE) {
+                        if (operation == 0) {
+                            GLOBAL_VARIABLE.incrementAndGet();
+                        } else {
+                            GLOBAL_VARIABLE.incrementAndGet();
+                        }
+                    }
+                }
+            }).start();
+        }
+
+        try {
+            Thread.sleep(TimeUnit.SECONDS.toMillis(duration));
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+
+        System.out.println("Result: " + GLOBAL_VARIABLE);
+        System.exit(0);
+    }
+
+    private static int readNumberOfThreads() {
+        int n = 0;
+
+        try {
+            System.out.print("Enter number of threads: ");
+            n = SCANNER.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("You should provide a number!");
+        }
+
+        return n;
+    }
+
+    private static int readDuration() {
+        int n = 0;
+
+        try {
+            System.out.print("Enter duration [seconds]: ");
+            n = SCANNER.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("You should provide a number!");
+        }
+
+        return n;
+    }
+}
