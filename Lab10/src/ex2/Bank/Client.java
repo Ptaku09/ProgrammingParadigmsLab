f201de764f01788209e020d1ad77049838b23c89
Mateusz Ptak <ptakm02@gmail.com>
2023-01-19 23:19:46 +0100

    lab 10 - update exercise 2

--- Lab10/src/ex2/Bank/Client.java
+++ Lab10/src/ex2/Bank/Client.java
@@ -8,7 +8,8 @@ import java.util.concurrent.TimeUnit;
 public class Client {
     private static final float MIN_AMOUNT = 200f;
     private static final float MAX_AMOUNT = 10000f;
-    private static final int OPERATION_INTERVAL_TIME = 500;
+    private static final int MIN_OPERATION_INTERVAL_TIME = 100;
+    private static final int MAX_OPERATION_INTERVAL_TIME = 500;
     private static final Random RAND = new Random();
     private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
     private final Account account;
@@ -23,7 +24,7 @@ public class Client {
 
     public void start() {
         executor.scheduleAtFixedRate(() -> {
-            synchronized (RAND) {
+            synchronized (Bank.ACCOUNTS) {
                 switch (getRandomOperation()) {
                     case DEPOSIT -> account.deposit(RAND.nextFloat(MIN_AMOUNT, MAX_AMOUNT));
                     case WITHDRAW -> account.withdraw(RAND.nextFloat(MIN_AMOUNT, MAX_AMOUNT));
@@ -35,7 +36,7 @@ public class Client {
                     }
                 }
             }
-        }, 0, OPERATION_INTERVAL_TIME, TimeUnit.MILLISECONDS);
+        }, 0, RAND.nextInt(MIN_OPERATION_INTERVAL_TIME, MAX_OPERATION_INTERVAL_TIME), TimeUnit.MILLISECONDS);
     }
 
     public void stop() {
